1b41edbbb108deb7809a274f0c2fd5c0e2afb93f
Richard Vašek <richard.vasek@tieto.com>
2018-01-02 20:11:56 +0100

    Add some unit tests

---
+++ src/test/java/cz/richiewenn/imp2fun/CfgInEdgesFillerTest.kt
@@ -0,0 +1,10 @@
+package cz.richiewenn.imp2fun
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CfgInEdgesFillerTest {
+
+    @org.junit.jupiter.api.Test
+    fun fill() {
+    }
+}
---
+++ src/test/java/cz/richiewenn/imp2fun/CfgJumpOptimizerTest.kt
@@ -0,0 +1,12 @@
+package cz.richiewenn.imp2fun
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CfgJumpOptimizerTest {
+
+    @Test
+    fun optimize() {
+    }
+}
